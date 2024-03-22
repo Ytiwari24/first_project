c020a03e6705e3ab5aac23443fd67cfa90ad0d33
Pravind <parvindraj004@gmail.com>
2024-03-22 18:37:50 +0530

    changes

--- src/cats/cat.entity.ts
+++ src/cats/cat.entity.ts
@@ -1,4 +1,30 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+// import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+// @Entity()
+// export class Cat {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+
+//   @Column({ nullable: true })
+//   profile_picture: string;
+
+//   @Column()
+//   name: string;
+
+//   @Column()
+//   age: number;
+
+//   @Column()
+//   breed: string;
+// }
+
+
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Timestamp } from 'typeorm';
+
+enum CatStatus {
+  ACTIVE = 1,
+  INACTIVE = 0,
+}
 
 @Entity()
 export class Cat {
@@ -16,4 +42,13 @@ export class Cat {
 
   @Column()
   breed: string;
+
+  // @Column({ enum: CatStatus, default: CatStatus.ACTIVE })
+  // status: CatStatus;
+
+  // @CreateDateColumn({ name: 'created_at' })
+  // createdAt: Timestamp;
+
+  // @UpdateDateColumn({ name: 'updated_at' })
+  // updatedAt: Timestamp;
 }
--- src/cats/cats.controller.spec.ts
+++ src/cats/cats.controller.spec.ts
@@ -1,63 +1,18 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Put,
-  Delete,
-  Param,
-  Body,
-} from '@nestjs/common';
-import { CatsService } from './cats.service';
-import { Cat } from './cat.entity';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CatsController } from './cats.controller';
 
-@Controller('cats')
-export class CatsController {
-  constructor(private readonly catsService: CatsService) {}
+describe('CatsController', () => {
+  let controller: CatsController;
 
-  @Get()
-  async findAll(): Promise<Cat[]> {
-    return this.catsService.findAll();
-  }
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CatsController],
+    }).compile();
 
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Cat> {
-    return this.catsService.findOne(Number(id));
-  }
+    controller = module.get<CatsController>(CatsController);
+  });
 
-  @Post()
-  async create(@Body() cat: Cat): Promise<Cat> {
-    return this.catsService.create(cat);
-  }
-
-  @Put(':id')
-  async update(@Param('id') id: string, @Body() cat: Cat): Promise<Cat> {
-    return this.catsService.update(Number(id), cat);
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') id: string): Promise<void> {
-    return this.catsService.remove(Number(id));
-  }
-}
-
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { CatsController } from './cats.controller';
-
-// describe('CatsController', () => {
-//   let controller: CatsController;
-
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       controllers: [CatsController],
-//     }).compile();
-
-//     controller = module.get<CatsController>(CatsController);
-//   });
-
-//   it('should be defined', () => {
-//     expect(controller).toBeDefined();
-//   });
-
-// });
-
-// src/cats/cats.controller.ts
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
